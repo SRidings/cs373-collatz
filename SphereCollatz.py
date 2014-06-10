66dfb48ffa760bc0a2e5db663e981308fee425c0
Stephen Ridings <sridings777@aol.com>
2014-06-09 22:06:32 -0500

    Third commit - first version of SphereCollatz

---
+++ SphereCollatz.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2014
+# Glenn P. Downing
+# ---------------------------
+
+# ------------
+# collatz_read
+# ------------
+
+def collatz_read (r) :
+    """
+    read two ints
+    r is a reader
+    return a list of the two ints, otherwise a list of zeros
+    """
+    s = r.readline()
+    if s == "" :
+        return []
+    a = s.split()
+    return [int(v) for v in a]
+
+# ------------
+# collatz_eval
+# ------------
+
+def collatz_eval (i, j) :
+    """
+    i is the beginning of the range, inclusive
+    j is the end       of the range, inclusive
+    return the max cycle length in the range [i, j]
+    """
+    assert i > 0
+    assert j > 0
+
+    t = i
+    max = -1
+
+    while (t <= j) :
+
+        n = t   
+        assert n > 0
+        c = 1
+        while (n > 1):
+            if (n % 2) == 0 :
+                n = (n / 2)
+            else :
+                n = (3 * n) + 1
+            c += 1
+        assert c > 0
+       # print(t, " is " , c)
+        if (c > max) :
+            max = c
+        t += 1
+
+    return max
+
+# -------------
+# collatz_print
+# -------------
+
+def collatz_print (w, i, j, v) :
+    """
+    print three ints
+    w is a writer
+    i is the beginning of the range, inclusive
+    j is the end       of the range, inclusive
+    v is the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+def collatz_solve (r, w) :
+    """
+    read, eval, print loop
+    r is a reader
+    w is a writer
+    """
+    while True :
+        a = collatz_read(r)
+        if not a :
+            return
+        i, j = a
+        v = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+#!/usr/bin/env python3
+
+# ------------------------------
+# projects/collatz/RunCollatz.py
+# Copyright (C) 2014
+# Glenn P. Downing
+# ------------------------------
+
+"""
+To run the program
+    % coverage3 run --branch RunCollatz.py < RunCollatz.in
+
+To obtain coverage of the run:
+    % coverage3 report -m
+
+To document the program
+    % pydoc -w Collatz
+"""
+
+# -------
+# imports
+# -------
+
+import sys
+
+# ----
+# main
+# ----
+
+collatz_solve(sys.stdin, sys.stdout)
+
+"""
+% coverage3 run --branch RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+
+
+% coverage3 report -m
+Name          Stmts   Miss Branch BrMiss  Cover   Missing
+---------------------------------------------------------
+Collatz         18      0      6      0   100%
+RunCollatz       5      0      0      0   100%
+---------------------------------------------------------
+TOTAL           23      0      6      0   100%
+"""
+
